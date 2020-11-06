f58e3bde90012810acca060c9c85d963a52b6332
Taink <minemax579@gmail.com>
2020-11-06 16:23:21 +0100

    Update file structure

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ import { join, dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const app = express();
 const PORT = 8080;
 
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'public', 'index.html'));
+    res.sendFile('public/index.html');
 });
 
 app.listen(PORT, () => {
