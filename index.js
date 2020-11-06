14ef0cd379209882330c208776e17f15fc9df67c
Taink <minemax579@gmail.com>
2020-11-06 15:25:36 +0100

    Add webpage

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express();
 const PORT = 8080;
 
 app.get('/', (req, res) => {
-    //
+    res.sendFile(join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}...`);
 });
