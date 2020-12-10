b8d4a45ed235375708c27dfc92d8daa2341eae20
Taink <minemax579@gmail.com>
2020-12-10 20:25:25 +0100

    Petits fix sur les routes

--- routes/login.js
+++ routes/login.js
@@ -3,16 +3,18 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 /* GET register page. */
-router.get('/', function(req, res, next) {
-    res.send(require('../pages/register')(
-    
-    ))
+router.get('/', function (req, res, next) {
+	res.send(require('../pages/login')());
 });
 
 /* POST register page. */
-router.post('/', function(req, res, next) {
-    let { Username: name, Password: pass } = req.body;
-    res.send(`Password for user ${name}: ${pass}`);
+router.post('/', function (req, res, next) {
+	let { Username: name, Password: pass } = req.body;
+	// let boolean = userController.login(name, pass)
+	// redirect to profile page if name and pass are correct
+	// otherwise redirect to login saying it was incorrect
+	//
+	res.send(`Password for user ${name}: ${pass}`);
 });
 
 module.exports = router;
--- routes/register.js
+++ routes/register.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 /* GET register page. */
-router.get('/', function(req, res, next) {
-    res.send(require('../pages/register')(
-		
-    ))
+router.get('/', function (req, res, next) {
+	res.send(require('../pages/register')());
 });
 
 /* POST register page. */
-router.post('/', function(req, res, next) {
-    let { Username: name, Password: pass, RepPassword: rpass } = req.body;
-    res.redirect('/login');
-    res.send(`Password for user ${name}: ${pass} (confirmation: ${rpass})`);
+router.post('/', function (req, res, next) {
+	let { Username: name, Password: pass, RepPassword: rpass } = req.body;
+	res.redirect('/login');
+	res.send(
+		`Password for user ${name}: <pre>${pass}</pre> (confirmation: ${rpass})`
+	);
 });
 
 module.exports = router;
