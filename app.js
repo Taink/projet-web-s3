8c677ff3153acc5995287c02d4166b36d9e19cd7
Taink <minemax579@gmail.com>
2020-12-10 19:18:51 +0100

    Useless lines are useless

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const fs = require('fs');
 const multer = require('multer');
-
 
 const { secret } = require('./config.json');
 const routes = require('./routes/index');
