a446f35ed8851d6568f9e8a291d7821fea6d1f7a
Taink <minemax579@gmail.com>
2020-12-10 20:02:16 +0100

    Fix de la DB

--- db/db.js
+++ db/db.js
@@ -1,6 +1,10 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+const sqlite3 = require("sqlite3").verbose();
+const { createUserTable } = require("./models/User");
 
-const db = new sqlite3.Database(path.join(__dirname, 'db.sqlite'), (err) => {
-    createUserTable(db);
-})
+const db = new sqlite3.Database(process.env.DB_PATH, (err) => {
+  if (err) console.error(err);
+
+  createUserTable(db);
+});
+
+module.exports = db;
--- db/models/User.js
+++ db/models/User.js
@@ -6,45 +6,52 @@ const saltRounds = 10;
  * @param {Database} db
  */
 const createUserTable = (db) => {
-   let stmt= db.prepare(`CREATE TABLE IF NOT EXISTS users ( 
+    let stmt = db.prepare(`CREATE TABLE IF NOT EXISTS users ( 
         user_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name NVARCHAR(25) UNIQUE NOT NULL,
         password TEXT NOT NULL
-        )`, (err) =>{
+        )`, (err) => {
             if (err) {
-                console.log("table already exists"); 
+                console.error("table already exists");
             } else {
                 console.log("Creation successful");
             }
-        })
-    stmt.run
-}
-const insertUser = (db,name,password) => {
-    let stmt =db.prepare(`INSERT INTO users (name,password) VALUES(?,?) `);
-    bcrypt.hash(password,saltRounds,function(err,hash){
-        stmt.run(name,hash);
     })
-}
+    stmt.run();
+};
 
-const confirmUser = (db,name,password)=>{ //return true si l'utilisateur existe et que le password est bon
-    let stmt= db.prepare(`SELECT name Name,password Password FROM users WHERE name=?`);
-    db.get(stmt,name,(err,row)=>{
-        if (err){
+const insertUser = (db, name, password) => {
+    let stmt = db.prepare(`INSERT INTO users (name,password) VALUES(?,?) `);
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+        stmt.run(name, hash);
+    })
+};
+
+const confirmUser = (db, name, password) => { //return true si l'utilisateur existe et que le password est bon
+    let stmt = db.prepare(`SELECT name Name,password Password FROM users WHERE name=?`);
+    db.get(stmt, name, (err, row) => {
+        if (err) {
             console.error("Username/password not found");
             return false;
         }
-        else{
+        else {
             let dbHash = row.password;
-            bcrypt.compare(password,dbHash,function(err,result){
-                if (result==true){
+            bcrypt.compare(password, dbHash, function (err, result) {
+                if (result === true) {
                     console.log("it's a match :)");
                     return true;
                 }
-                else{
-                    console.error("Too bad :(");
+                else {
+                    console.error("Wrong password :(");
                     return false;
                 }
             })
         }
     })
+}
+
+module.exports = {
+    createUserTable,
+    insertUser,
+    confirmUser,
 }
