2ee7d1e064265652b93c1b62d1d9a460c5496598
Taink <minemax579@gmail.com>
2020-12-12 20:35:03 +0100

    Remove useless import

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const registerRouter = require('./register');
 const loginRouter = require('./login');
 
-const { html } = require('common-tags');
-
 const router = express.Router();
 
 /* GET home page. */
