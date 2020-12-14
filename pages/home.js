738775d078bc1166f57cd9c2952e228b63e1b023
Taink <minemax579@gmail.com>
2020-12-14 19:18:40 +0100

    prettier :^)

--- pages/home.js
+++ pages/home.js
@@ -1,51 +1,46 @@
 const { html, safeHtml } = require('common-tags');
 
-function getContent(username =''){
-  return html`
-    ${require('../templates/head')(
-      'velux - homepage',
-      'css/styles.css',
-      'Velux user home page'
-    )}
-      <script>
-      function initElement()
-      {
-        let p = document.getElementById("onClic");
-        p.onclick = showAlert;
-      }
-      function showAlert()
-      {
-        alert("Still nothing here");
-      }
-      </script>
+function getContent(username = '') {
+	return html`
+		${require('../templates/head')(
+			'velux - homepage',
+			'css/styles.css',
+			'Velux user home page'
+		)}
+		<script>
+			function initElement() {
+				let p = document.getElementById('onClic');
+				p.onclick = showAlert;
+			}
+			function showAlert() {
+				alert('Still nothing here');
+			}
+		</script>
 
-      <h1>Welcome back 
-      ${
-        username
-          ? safeHtml`
+		<h1>
+			Welcome back
+			${username
+				? safeHtml`
             <p class="form-error"
               There was an error while printing your username :
               ${username}
               </p>
             `
-          : ''
-      }
-      ! </h1>
+				: ''}
+			!
+		</h1>
 
+		<p>You can get bored all the time you want on this website :)</p>
 
-      <p>You can get bored all the time you want on this website :)</p>
+		<p id="onClic">Clic here for a surprise :)</p>
 
-      <p id="onClic">Clic here for a surprise :)</p>
+		<nav>
+			<a href="/index">Log out</a>
+			<a href="/option">Account option</a>
+		</nav>
 
-      <nav>
-      <a href="/index">Log out</a>
-      <a href="/option">Account option</a>
-		  </nav>
-
-
-
-    ${require('../templates/foot')()}
-    `;
+		${require('../templates/foot')()}
+	`;
 }
 
 module.exports = getContent;
